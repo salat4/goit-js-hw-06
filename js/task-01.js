22469b15911ccc86c50e95f1a66c1e49e570c87a
Dima Malik <mdima4266@gmail.com>
2021-12-06 21:12:02 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-const categories = Document.querySelectorAll('.item');
+const categories = document.querySelectorAll('.item');
 console.log('Number of categories:', categories.length); 
 const item = document.querySelectorAll('.item');
 item.forEach((e) => console.log('Category:', e.firstElementChild.textContent, 'Elements:', e.lastElementChild.children.length));
